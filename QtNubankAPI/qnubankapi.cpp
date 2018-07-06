6b07e1147eab3d4af2aba2bb7fb683406eeeb1be
Israel Lins Albuquerque <israel@proabakus.com.br>
2018-07-06 13:02:17 -0300

    defining API and starting implementation

--- QtNubankAPI/qnubankapi.cpp
+++ QtNubankAPI/qnubankapi.cpp
@@ -1,6 +1,36 @@
 #include "qnubankapi.h"
 
+class QNubankAPIPrivate {
+    Q_DISABLE_COPY(CoordinateDialogPrivate)
+    Q_DECLARE_PUBLIC(CoordinateDialog)
+    QNubankAPI* const q_ptr;
 
-QNubankAPI::QNubankAPI()
+    QNubankAPIPrivate(QNubankAPI* q) : q_ptr(q) {}
+
+    QString discoveryUrl = "https://prod-s0-webapp-proxy.nubank.com.br/api/discovery";
+    QMap<QString, QString> defaultHeader = {
+        { "Content-Type", "application/json"},
+        { "X-Correlation-Id", "WEB-APP.pewW9"},
+        { "User-Agent", "QtNubankAPI - www.github.com/israelins/QtNubankAPI"},
+    };
+    QMap<QString, QString> proxyUrls;
+
+    bool discoverUrls() {
+        static QString discoveryUrl = "https://prod-s0-webapp-proxy.nubank.com.br/api/discovery";
+    }
+};
+
+QNubankAPI::QNubankAPI(QObject* parent) :
+    QObject(parent),
+    d_ptr(new QNubankAPIPrivate(this))
+{}
+
+void QNubankAPI::authenticate(const QNubankAPI::AuthRequest& request, std::function<void (const QNubankAPI::Response&)> onFinished)
 {
+    authenticate(this, request, onFinished);
+}
+
+void QNubankAPI::authenticate(QObject* context, const QNubankAPI::AuthRequest& request, std::function<void (const QNubankAPI::Response&)> onFinished)
+{
+
 }
--- QtNubankAPI/qnubankapi.h
+++ QtNubankAPI/qnubankapi.h
@@ -1,12 +1,57 @@
 #ifndef QNUBANKAPI_H
 #define QNUBANKAPI_H
 
+#include <QObject>
+#include <QString>
 
-class QNubankAPI
+#include <QJsonDocument>
+
+class QNubankAPIPrivate;
+class QNubankAPI : public QObject
 {
+  Q_OBJECT
+  Q_DECLARE_PRIVATE(QNubankAPI)
+  QScopedPointer<QNubankAPIPrivate> const d_ptr;
 
 public:
-    QNubankAPI();
+    explicit QNubankAPI(QObject* parent);
+
+    struct Response {
+        bool sucess;
+        QJsonValue data;
+    };
+
+    struct AuthRequest {
+        QString cpf;
+        QString password;
+
+        AuthRequest()
+        {}
+
+        AuthRequest(const QString& cpf = QString(), const QString& password = QString()) :
+            cpf(cpf), password(password)
+        {}
+    };
+
+    void authenticate(const QNubankAPI::AuthRequest& request, std::function<void (const QNubankAPI::Response&)> onFinished);
+    void authenticate(QObject* context, const QNubankAPI::AuthRequest& request, std::function<void (const QNubankAPI::Response&)> onFinished);
+
+    void setAccessToken(const QString& accessToken);
+
+    void cardFeed(std::function<void (const QNubankAPI::Response&)> a_onFinished);
+    void cardFeed(QObject* context, std::function<void (const QNubankAPI::Response&)> a_onFinished);
+
+    void bills(std::function<void (const QNubankAPI::Response&)> a_onFinished);
+    void bills(QObject* context, std::function<void (const QNubankAPI::Response&)> a_onFinished);
+
+    void billDetails(QJsonValue bill, std::function<void (const QNubankAPI::Response&)> a_onFinished);
+    void billDetails(QObject* context, QJsonValue bill, std::function<void (const QNubankAPI::Response&)> a_onFinished);
+
+    void accountFeed(std::function<void (const QNubankAPI::Response&)> a_onFinished);
+    void accountFeed(QObject* context, std::function<void (const QNubankAPI::Response&)> a_onFinished);
+
+    void accountBalance(std::function<void (const QNubankAPI::Response&)> a_onFinished);
+    void accountBalance(QObject* context, std::function<void (const QNubankAPI::Response&)> a_onFinished);
 };
 
 #endif // QNUBANKAPI_H
